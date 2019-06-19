b6f2b272303af98114c02f748a261d4a2389f62a
yanll <liangliang.yan@yeepay.com>
2019-06-19 15:09:55 +0800

    BOSS commit

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -2,4 +2,5 @@ echo 'build start'
 docker build -f Dockerfile -t techforge/baseos:beta .
 echo 'build success'
 #docker run --name baseos -i -t -v /usr/local/software/:/mnt/software/ techforge/baseos:beta /bin/bash
+#docker run --name baseos -i -t -v /usr/local/software/:/mnt/software/ techforge/baseos:beta /usr/sbin/init
 
