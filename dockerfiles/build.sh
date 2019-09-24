e74ca0f81ebd656efff7e2e967f170f7e4c7b086
yanll <liangliang.yan@yeepay.com>
2019-09-24 17:07:35 +0800

    commit push

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -4,3 +4,6 @@ echo 'build success'
 #docker run --name baseos -p 18080:8080 -i -t -v /usr/local/software/:/mnt/software/ techforge/baseos:beta /bin/bash
 #docker run --name baseos -p 18080:8080 -i -t -v /usr/local/software/:/mnt/software/ techforge/baseos:beta /usr/sbin/init
 
+#docker run -d -i -t <imageID> /bin/bash
+
+
